328e9e12c163d5d08023a0ed5800fc5153483539
sagurah <satyogag@gmail.com>
2022-09-06 22:58:03 +0700

    btnClear & btnLogin

--- app/src/main/java/com/example/tubes/MainActivity.kt
+++ app/src/main/java/com/example/tubes/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -12,10 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var username : TextInputEditText
     private lateinit var password : TextInputEditText
+    private lateinit var mainLayout : ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainLayout = findViewById(R.id.mainLayout)
 
         setText()
 
@@ -24,10 +29,50 @@ class MainActivity : AppCompatActivity() {
             val moveRegistration = Intent(this, RegisterActivity::class.java)
             startActivity(moveRegistration)
         })
+
+        val btnClear : Button = findViewById(R.id.btnClear)
+        btnClear.setOnClickListener(View.OnClickListener{
+            username = findViewById(R.id.textUsername)
+            password = findViewById(R.id.textPassword)
+
+            Snackbar.make(mainLayout, "Text Cleared Successfully", Snackbar.LENGTH_LONG).show()
+            username.setText("")
+            password.setText("")
+        })
+
+        val btnLogin : Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener(View.OnClickListener{
+            var checkLogin : Boolean
+            username = findViewById(R.id.textUsername)
+            password = findViewById(R.id.textPassword)
+            val bundle = intent.extras
+
+            var uname = username.text.toString()
+            var pass = password.text.toString()
+
+            if(uname.isEmpty())
+                username.setError("Username must be filled with text")
+                checkLogin = false
+
+            if(pass.isEmpty())
+                password.setError("Password must be filled with text")
+                checkLogin = false
+
+            if (bundle != null) {
+                if(
+                    uname == bundle.getString("username")
+                    && pass == bundle.getString("password")
+                ) checkLogin = true
+            }
+
+            if(!checkLogin)
+                return@OnClickListener
+            else
+                Snackbar.make(mainLayout, "Anda berhasil login!", Snackbar.LENGTH_LONG).show() // Tinggal diganti Intent ke layout MainMenu
+        })
     }
 
     fun setText() {
-
         username = findViewById(R.id.textUsername)
         password = findViewById(R.id.textPassword)
 
