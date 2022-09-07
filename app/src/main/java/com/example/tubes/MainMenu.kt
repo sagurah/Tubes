208f6421947c8bab9ee1d8de9e0795d2edbdce0c
Vallent11 <111846775+Vallent11@users.noreply.github.com>
2022-09-07 17:14:10 +0700

    Bottom Nav View

--- app/src/main/java/com/example/tubes/MainMenu.kt
+++ app/src/main/java/com/example/tubes/MainMenu.kt
@@ -2,11 +2,35 @@ package com.example.tubes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tubes.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainMenu : AppCompatActivity() {
 
+    private lateinit var binding : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main_menu)
+        replaceFragment(HomeFragment())
+        binding = findViewById(R.id.bottomNavigationView)
+        binding.setOnItemSelectedListener{ item->
+            when(item.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+                R.id.plans -> replaceFragment(PlansFragment())
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
+
