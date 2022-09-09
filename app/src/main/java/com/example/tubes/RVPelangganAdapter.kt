b2600daea24ee681ee05fe001a6bfa41ee737b23
DopePrince <theys100@gmail.com>
2022-09-09 20:14:58 +0700

    Tambahan

---
+++ app/src/main/java/com/example/tubes/RVPelangganAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.tubes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tubes.entity.Pelanggan
+import org.w3c.dom.Text
+
+class RVPelangganAdapter(private val data: Array<Pelanggan>) : RecyclerView.Adapter<RVPelangganAdapter.viewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rv_item_pelanggan, parent, false)
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val currentItem = data[position]
+        holder.tvNama.text = currentItem.name
+        holder.tvUmur.text = currentItem.umur
+        holder.tvTelp.text = currentItem.noTelp
+        holder.tvMulai.text = currentItem.tglMulai
+        holder.tvAkhir.text = currentItem.tglAkhir
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class viewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val tvNama : TextView = itemView.findViewById(R.id.namaPelanggan)
+        val tvUmur : TextView = itemView.findViewById(R.id.umurPelanggan)
+        val tvTelp : TextView = itemView.findViewById(R.id.noTelpPelanggan)
+        val tvMulai : TextView = itemView.findViewById(R.id.tglMulai)
+        val tvAkhir : TextView = itemView.findViewById(R.id.tglAkhir)
+    }
+}
