8061ca24ce589852a8369642011eb7b3de550765
sagurah <satyogag@gmail.com>
2022-09-23 15:20:18 +0700

    mendorong profile logic

--- app/src/main/java/com/example/tubes/main_fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/tubes/main_fragment/ProfileFragment.kt
@@ -1,11 +1,19 @@
 package com.example.tubes.main_fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tubes.*
+import com.example.tubes.databinding.FragmentProfileBinding
+import com.example.tubes.room.UserDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,41 +26,38 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    val db by lazy { activity?.let {}}
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val db by lazy { activity?.let { UserDB(it) }}
+    private val id = "idKey"
+    private val preference = "myPref"
+    var sharedPreferences: SharedPreferences? = null
+    private var bind: FragmentProfileBinding? = null
+    private val binding get() = bind!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        bind = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences = activity?.getSharedPreferences(preference, Context.MODE_PRIVATE)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val user = db?.UserDAO()?.getUser(sharedPreferences!!.getString(id, "")!!.toInt())?.get(0)
+            binding.tvUsername.setText(user?.username)
+            binding.tvEmail.setText(user?.email)
+            binding.tvNomorTelp.setText(user?.nomorTelp)
+            binding.tvTanggalLahir.setText(user?.tglLahir)
+
+            binding.btnEdit.setOnClickListener{
+                val move = Intent(activity, //)
             }
+        }
+
     }
 }
