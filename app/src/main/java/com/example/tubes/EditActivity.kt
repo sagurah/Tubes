c1cc634d352562d8300ab612f7764963db153ff1
sagurah <satyogag@gmail.com>
2022-09-23 19:49:46 +0700

    Update EditActivity.kt

--- app/src/main/java/com/example/tubes/EditActivity.kt
+++ app/src/main/java/com/example/tubes/EditActivity.kt
@@ -3,11 +3,14 @@ package com.example.tubes
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import com.example.tubes.room.UserDB
 import com.example.tubes.*
 import com.example.tubes.databinding.ActivityEditBinding
 import com.example.tubes.databinding.FragmentProfileBinding
+import com.example.tubes.main_fragment.ProfileFragment
 import com.example.tubes.room.user.User
 import kotlinx.android.synthetic.main.activity_edit.*
 import kotlinx.coroutines.CoroutineScope
@@ -18,8 +21,8 @@ class EditActivity : AppCompatActivity() {
     private val id = "id"
     private val preference = "myPref"
     var sharedPreferences : SharedPreferences? = null
-    private var bind: ActivityEditBinding? = null
-    private val binding get() = bind!!
+//    private var bind: ActivityEditBinding? = null
+//    private val binding get() = bind!!
 
     val db by lazy { UserDB(this) }
     private val userDAO = db.UserDAO()
@@ -29,17 +32,27 @@ class EditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit)
 
         btnEditProfile.setOnClickListener(){
-            val username: String = binding?.tilUsername?.getEditText()?.getText().toString()
-            val password: String = binding?.tilPassword?.getEditText()?.getText().toString()
-            val email: String = binding?.tilEmail?.getEditText()?.getText().toString()
-            val tglLahir: String = binding?.tilTglLahir?.getEditText()?.getText().toString()
-            val noTelp: String = binding?.tilNoTelp?.getEditText()?.getText().toString()
+            val username: String = findViewById<View?>(R.id.tilUsername).toString()
+            val password: String = findViewById<View?>(R.id.tilPassword).toString()
+            val email: String = findViewById<View?>(R.id.tilEmail).toString()
+            val tglLahir: String = findViewById<View?>(R.id.tilTglLahir).toString()
+            val noTelp: String = findViewById<View?>(R.id.tilNoTelp).toString()
 
             CoroutineScope(Dispatchers.IO).launch {
                 val user = User(0, username, password, tglLahir, email, noTelp)
                 userDAO.addUser(user)
+
+                replaceFragment(ProfileFragment())
             }
         }
+
+    }
+    fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 
 }
