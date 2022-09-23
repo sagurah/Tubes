641f944cff1a43c662dfe4d09455beaa8a91ca69
sagurah <satyogag@gmail.com>
2022-09-23 15:46:43 +0700

    fragmen profil

--- app/src/main/java/com/example/tubes/main_fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/tubes/main_fragment/ProfileFragment.kt
@@ -15,16 +15,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
     val db by lazy { activity?.let { UserDB(it) }}
     private val id = "idKey"
@@ -55,7 +45,9 @@ class ProfileFragment : Fragment() {
             binding.tvTanggalLahir.setText(user?.tglLahir)
 
             binding.btnEdit.setOnClickListener{
-                val move = Intent(activity, //)
+                val move = Intent(activity, EditActivity::class.java)
+                startActivity(move)
+                activity?.finish()
             }
         }
 
