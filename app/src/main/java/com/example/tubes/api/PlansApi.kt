90f004c2021466493df558c4399834ddfb735763
sagurah <satyogag@gmail.com>
2022-11-20 19:58:22 +0700

    Database & API done

--- app/src/main/java/com/example/tubes/api/PlansApi.kt
+++ app/src/main/java/com/example/tubes/api/PlansApi.kt
@@ -2,7 +2,7 @@ package com.example.tubes.api
 
 class PlansApi {
     companion object{
-        val BASE_URL = ""
+        val BASE_URL = "https://tubespbp-api.herokuapp.com/"
         val GET_ALL_URL = BASE_URL + "plans/"
         val GET_BY_ID_URL = BASE_URL + "plans/"
         val ADD_URL = BASE_URL + "plans/"
---
+++ app/src/main/java/com/example/tubes/api/UserApi.kt
@@ -0,0 +1,12 @@
+package com.example.tubes.api
+
+class UserApi {
+    companion object{
+        val BASE_URL = "https://tubespbp-api.herokuapp.com/"
+        val GET_ALL_URL = BASE_URL + "user/"
+        val GET_BY_ID_URL = BASE_URL + "user/"
+        val ADD_URL = BASE_URL + "user/"
+        val UPDATE_URL = BASE_URL + "user/"
+        val DELETE_URL = BASE_URL + "user/"
+    }
+}
