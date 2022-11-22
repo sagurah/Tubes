20de85f1964d104eca695e718bc93d75ef0fb692
sagurah <satyogag@gmail.com>
2022-11-22 16:02:47 +0700

    PDF Belum jadi

--- app/src/main/java/com/example/tubes/RVPlanAdapter.kt
+++ app/src/main/java/com/example/tubes/RVPlanAdapter.kt
@@ -1,22 +1,24 @@
 package com.example.tubes
 
 import android.content.Context
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Filter
-import android.widget.Filterable
-import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tubes.main_fragment.PlansFragment
 import com.example.tubes.room.jadwal.Jadwal
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.File
+import java.io.FileOutputStream
 import java.util.*
 import kotlin.collections.ArrayList
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.lowagie.text.PageSize
+import com.lowagie.text.Paragraph
 
 //class RVPlanAdapter(private val data: Array<Jadwal>) : RecyclerView.Adapter<RVPlanAdapter.viewHolder>() {
 class RVPlanAdapter(
@@ -58,6 +60,21 @@ class RVPlanAdapter(
                 }
                 .show()
         }
+
+        holder.btnEdit.setOnClickListener{
+
+        }
+
+        holder.btnCetak.setOnClickListener{
+            val dialog = MaterialAlertDialogBuilder(context)
+            dialog.setTitle("Konfirmasi")
+                .setMessage("Apakah anda yakin untuk membuat PDF?")
+                .setNegativeButton("Batal", null)
+                .setPositiveButton("Buat"){_,_ ->
+                    createPdf(namaTrainer, jamMulai, jamAkhir, currentItem.id)
+                }
+                .show()
+        }
     }
 
     fun setData(data2: Array<Jadwal>) {
@@ -75,6 +92,7 @@ class RVPlanAdapter(
         val tvAkhir : TextView = itemView.findViewById(R.id.jamAkhir)
         val btnDelete : Button = itemView.findViewById(R.id.btnDelete)
         val btnEdit : Button = itemView.findViewById(R.id.btnEdit)
+        val btnCetak : Button = itemView.findViewById(R.id.btnCetak)
     }
     override fun getFilter(): Filter {
         return object : Filter (){
@@ -104,6 +122,43 @@ class RVPlanAdapter(
             }
         }
 
+    }
+
+    private fun createPdf(namaTrainer: String, plan: String, jamMulai: String, jamAkhir: String, index: Int){
+        val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString()
+        val file = File(pdfPath, "Jadwal_Latihan_" + index + ".pdf")
+        FileOutputStream(file)
+
+        val writer = PdfWriter(file)
+        val pdfDoc = PdfDocument(writer)
+        val doc = Document(pdfDoc)
+        pdfDoc.defaultPageSize = PageSize.A4
+        doc.setMargins(5f, 5f, 5f, 5f)
+
+        val width = floatArrayOf(100f, 100f)
+        val table = Table(width)
+
+        val group = Paragraph(
+            """
+                Data Workout Plans: 
+            """.trimIndent()).setTextAlignment(TextAlignment.CENTER).setFontSize(12f)
+
+        table.setHorizontalAlignment(HorizontalAlignment.CENTER)
+        table.addCell(Cell().add(Paragraph("Nama Trainer")))
+        table.addCell(Cell().add(Paragraph(namaTrainer)))
+        table.addCell(Cell().add(Paragraph("Workout Plan")))
+        table.addCell(Cell().add(Paragraph(plan)))
+        table.addCell(Cell().add(Paragraph("Jam Mulai")))
+        table.addCell(Cell().add(Paragraph(jamMulai)))
+        table.addCell(Cell().add(Paragraph("Jam Akhir")))
+        table.addCell(Cell().add(Paragraph(jamAkhir)))
+
+        doc.add(group)
+        doc.add(table)
+
+        doc.close()
+
+        Toast.makeText(this, "PDF Created", Toast.LENGTH_SHORT).show()
     }
 
 }
